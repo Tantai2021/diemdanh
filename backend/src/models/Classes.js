27d126cf7701185f0d4d2669a7d3cc4b3c55f893
Tantai2021 <94042270+Tantai2021@users.noreply.github.com>
2025-04-08 20:28:30 +0700

    Update Classes.js

--- backend/src/models/Classes.js
+++ backend/src/models/Classes.js
@@ -5,7 +5,7 @@ const Class = sequelize.define("Class", {
     class_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     class_name: { type: DataTypes.STRING, allowNull: false },
     class_code: { type: DataTypes.STRING, allowNull: false },
-    teacher_id: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+    teacher_id: { type: DataTypes.INTEGER, allowNull: false },
     start_date: { type: DataTypes.DATE, allowNull: false },
     end_date: { type: DataTypes.DATE, allowNull: false }
 }, { timestamps: false });
